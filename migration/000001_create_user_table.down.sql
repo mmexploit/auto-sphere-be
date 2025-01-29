1d7755762201448caaa2881f4c2409afadee6359
mahider <95926290+Mahider-T@users.noreply.github.com>
2025-01-29 16:47:43 +0300

    feat: :sparkles: do migration

--- migration/000001_create_user_table.down.sql
+++ migration/000001_create_user_table.down.sql
@@ -1,10 +1,2 @@
-CREATE TYPE role AS ENUM ('ADMIN', 'OPERATOR', 'SALES');
-
-CREATE TABLE IF NOT EXISTS users {
-    id SERIAL PRIMARY KEY,
-    name text NOT NULL,
-    email UNIQUE text,
-    phone_number UNIQUE text NOT NULL,
-    role role NOT NULL,
-    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-}
+DROP TABLE IF EXISTS users;
+DROP TYPE IF EXISTS role;
--- migration/000001_create_user_table.up.sql
+++ migration/000001_create_user_table.up.sql
@@ -1,2 +1,10 @@
-DROP TABLE IF EXISTS users;
-DROP TYPE IF EXISTS role;
+CREATE TYPE role AS ENUM ('ADMIN', 'OPERATOR', 'SALES');
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE, -- Moved UNIQUE constraint after type definition
+    phone_number TEXT UNIQUE NOT NULL,
+    role role NOT NULL,
+    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
